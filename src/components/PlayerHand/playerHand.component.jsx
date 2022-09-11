935ad95ac2bb28a251b948f4d582a6432c983513
Khris Kempis <khriskempis@gmail.com>
2022-09-10 23:58:25 -0700

    add requiredResource component

--- src/components/PlayerHand/playerHand.component.jsx
+++ src/components/PlayerHand/playerHand.component.jsx
@@ -1,33 +1,74 @@
 import './playerHand.styles.scss'
 
 const PlayerHand = ({ player: { hand } }) => {
- return (
-  <div className="player-hand">
-    <h4>Hand</h4>
-    <ul className="player-hand__list">
-      {
-        hand && 
-        hand.map(({ id, name, requiredResource, produce, value }) => {
-        const { hay, clay, lumber, stone, wool } = requiredResource || [];
-        return (
-          <li className="player-hand__card" key={id}>
-            <h4>{name}</h4>
-            <div className="town__building--req-resource">
-              { hay && <p type="hay" anount={hay}>{hay} Hay</p> }
-              { clay && <p type="clay" anount={clay}>{clay} Clay</p> }
-              { lumber && <p type="lumber" anount={lumber}>{lumber} Lumber</p> }
-              { stone && <p type="stone" anount={stone}>{stone} Stone</p> }
-              { wool && <p type="wool" anount={wool}>{wool} Wool</p> }
-            </div>
-            <p>{produce}</p>
-            <p>{value}</p>
-          </li>
-          )
-        })
+  return (
+    <div className="player-hand">
+      <h4>Hand</h4>
+      <ul className="player-hand__list">
+        {
+          hand && 
+          hand.map(({ id, name, requiredResource, plusOneResource, produce, value }) => {
+          return (
+            <li className="player-hand__card" key={id}>
+              <h4>{name}</h4>
+              <div className="player-hand__resources">
+                {
+                  requiredResource && 
+                  renderRequiredResource(requiredResource)
+                }
+                {
+                  plusOneResource &&
+                  renderMarketOfficeResource(plusOneResource)
+                }
+              </div>
+              <p>{produce}</p>
+              <p>{value}</p>
+            </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+const renderRequiredResource = (requiredResource) => {
+  // TODO: add highest to lowest resource logic to display of resources
+  return (
+    <>
+      { 
+        Object.entries(requiredResource)
+          .filter(([ resource, amount ]) => {
+            return amount !== null;
+          })
+          .map(([ resource, amount ], index) => {
+              return(
+                <p 
+                  className="player-hand__required-resource"
+                  key={index}
+                  type={resource} 
+                  anount={amount}
+                >
+                    {amount} {resource.charAt(0).toUpperCase() + resource.slice(1)}
+                </p> 
+              )
+          })
       }
-    </ul>
-  </div>
- )
+    </>
+  )
+}
+
+const renderMarketOfficeResource = (resource) => {
+  return (
+    <>
+      <p
+        className="player-hand__plus-one-resource"
+        plusOne={resource}
+      >
+        +{resource}
+      </p>
+    </>
+  )
 }
 
 export default PlayerHand;
